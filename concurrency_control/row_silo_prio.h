6b5ad79642eb62b93a14b29e4fa0c261af64d48b
Sven Hwang <sven.chwen@gmail.com>
2021-12-05 15:39:05 -0600

    Squashed commit of the following:
    commit 91efb4816fab423d9e389abe5cf83991bee0ce35
    Author: Sven Hwang <sven.chwen@gmail.com>
    Date:   Sun Dec 5 15:34:38 2021 -0600
    
        cleanup and fix

--- concurrency_control/row_silo_prio.h
+++ concurrency_control/row_silo_prio.h
@@ -163,7 +163,6 @@ public:
 #if DEBUG_SVEN
 		printf("before unlock: latch-%1u, prio_ver-%1u, prio-%2u, ref_cnt-%1u, data_ver-%u\n", _tid_word_prio.is_locked(), _tid_word_prio.get_prio_ver(), _tid_word_prio.get_prio(), _tid_word_prio.get_ref_cnt(), _tid_word_prio.get_data_ver());
 #endif
-		// _tid_word_prio.release_prio(prio, prio_ver);
 		_tid_word_prio.unlock();
 #if DEBUG_SVEN
 		printf("after unlock: latch-%1u, prio_ver-%1u, prio-%2u, ref_cnt-%1u, data_ver-%u\n", _tid_word_prio.is_locked(), _tid_word_prio.get_prio_ver(), _tid_word_prio.get_prio(), _tid_word_prio.get_ref_cnt(), _tid_word_prio.get_data_ver());
