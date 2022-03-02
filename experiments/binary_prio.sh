0ea7bdfd4e3994a747e62c9ed0e7f5a81b0d31d6
Sven Hwang <sven.chwen@gmail.com>
2022-03-01 23:15:23 -0500

    update binary_prio.sh

--- experiments/binary_prio.sh
+++ experiments/binary_prio.sh
@@ -1,26 +1,31 @@
 cd ..
 
+for i in 0
+do
 for zipf in 0.9 0.99 1.2
 do
 for thd in 1 2 4 8 16 32 64
 do
 for alg in SILO SILO_PRIO
 do
-	python test.py experiments/binary_prio.json ZIPF_THETA=${zipf} THREAD_CNT=${thd} CC_ALG=${alg} > outputs/${alg}_YCSB_${zipf}_${thd}_output.txt
+	python test.py experiments/binary_prio.json ZIPF_THETA=${zipf} THREAD_CNT=${thd} CC_ALG=${alg} DUMP_LATENCY=false > outputs/${i}_${alg}_YCSB_${zipf}_${thd}_output.txt
 done
-	mv latency_dump.csv outputs/SILO_YCSB_${zipf}_${thd}_latency.csv
-	mv latency_dump.csv_zero outputs/SILO_PRIO_YCSB_${zipf}_${thd}_prio0_latency.csv
-	mv latency_dump.csv_one  outputs/SILO_PRIO_YCSB_${zipf}_${thd}_prio1_latency.csv
+	#mv latency_dump.csv outputs/SILO_YCSB_${zipf}_${thd}_latency.csv
+	#mv latency_dump.csv_zero outputs/SILO_PRIO_YCSB_${zipf}_${thd}_prio0_latency.csv
+	#mv latency_dump.csv_one  outputs/SILO_PRIO_YCSB_${zipf}_${thd}_prio1_latency.csv
+done
 done
 done
 
+for i in 0
+do
 for thd in 1 2 4 8 16 32 64
 do
 for alg in SILO SILO_PRIO
 do
-	python test.py experiments/binary_prio.json THREAD_CNT=${thd} CC_ALG=${alg} WORKLOAD=TPCC > outputs/${alg}_TPCC_${thd}_output.txt
+	python test.py experiments/binary_prio.json THREAD_CNT=${thd} CC_ALG=${alg} WORKLOAD=TPCC DUMP_LATENCY=false > outputs/${i}_${alg}_TPCC_${thd}_output.txt
 done
-	mv latency_dump.csv outputs/SILO_TPCC_${thd}_latency.csv
-	mv latency_dump.csv_zero outputs/SILO_PRIO_TPCC_${thd}_prio0_latency.csv
-	mv latency_dump.csv_one  outputs/SILO_PRIO_TPCC_${thd}_prio1_latency.csv
+	#mv latency_dump.csv outputs/SILO_TPCC_${thd}_latency.csv
+	#mv latency_dump.csv_zero outputs/SILO_PRIO_TPCC_${thd}_prio0_latency.csv
+	#mv latency_dump.csv_one  outputs/SILO_PRIO_TPCC_${thd}_prio1_latency.csv
 done
